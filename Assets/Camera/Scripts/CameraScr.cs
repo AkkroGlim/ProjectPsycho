05ff0c90dae83c287394b90253bbecb09dca26d4
AkkroGlim <111155608+AkkroGlim@users.noreply.github.com>
2022-10-29 00:07:05 +0300

    Commit 1.004

--- Assets/Camera/Scripts/CameraScr.cs
+++ Assets/Camera/Scripts/CameraScr.cs
@@ -8,39 +8,58 @@ public class CameraScr : MonoBehaviour
     private int distance = 10;
     private float focusRadius = 1f;
     private Vector3 focusPoint;
-    private float focusCentering = 0.5f;
-    private bool motherfuckingFlag = false;
+    private float focusCentering = 0.7f;
+    private bool focusFlag = false;
+    private float t = 1f;
+    private int reloadCamera = 0;
 
     private void Awake()
     {
         focusPoint = player.position;
     }
 
+    private void FixedUpdate()
+    {
+        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            reloadCamera++;
+            if(reloadCamera > 20)
+            {
+                reloadCamera = 0;
+                focusFlag = false;
+                t = 1f;
+            }
+        }
+    }
+
     private void LateUpdate()
     {
         UpdateFocusPoint();
         Vector3 lookDirection = transform.forward;
-        transform.localPosition = focusPoint - lookDirection * distance;
+        transform.position = focusPoint - lookDirection * distance;
     }
 
     private void UpdateFocusPoint()
     {
         Vector3 targetPoint = player.position;
         float distance = Vector3.Distance(targetPoint, focusPoint);
-        float t = 1f;
-        if (distance > 0.01f && !motherfuckingFlag)
-        {
-            t = Mathf.Pow(1f - focusCentering, Time.unscaledDeltaTime);
-        }
+        
         if (distance > focusRadius)
         {
-            motherfuckingFlag = true;
+            focusFlag = true;
+            t = 1;
         }
-        if (motherfuckingFlag)
+        if (distance > 0.01f && !focusFlag)
         {
-            t = Mathf.Min(t, focusRadius / distance);
+            t = Mathf.Pow(1f - focusCentering, Time.unscaledDeltaTime);
+            focusPoint = Vector3.Lerp(targetPoint, focusPoint, t);
         }
-        focusPoint = Vector3.Lerp(targetPoint, focusPoint, t);
+        else
+        {
+            t -= 0.006f;
+            focusPoint = Vector3.Lerp(targetPoint, focusPoint, t);            
+        }
+        //focusPoint = Vector3.Lerp(targetPoint, focusPoint, t);
     }
 }
 
